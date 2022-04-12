19cfe4fddecd7b551a1ec5c061f87d89ca494816
gowtham659 <gowthamthota121@gmail.com>
2022-04-12 12:21:40 +0530

    personel

---
+++ inheritence_basic.py
@@ -0,0 +1,15 @@
+class student:
+    def __init__(self,name,age) -> None:
+        self.n = name
+        self.a = age
+    def display(self):
+        print("name:%s and age:%d"%(self.n,self.a))
+s = student("gowtham",22)
+s1 = student("harsha",90)
+s2 = student("harish",39)
+s3 = student("sahith",88)
+
+s.display()
+s1.display()
+s2.display()
+s3.display()
